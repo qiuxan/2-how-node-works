0df7aaa96ec722fc88133ccb40eef9846140f883
qiuxan <qiuxan@gmail.com>
2023-08-18 22:49:08 +1000

    Requiring Modules in Practice

---
+++ modules.js
@@ -0,0 +1,17 @@
+// console.log(arguments)
+// console.log(require("module").wrapper);
+
+
+// const C = require("./test-module-1");
+// const calc1 = new C();
+// console.log(calc1.add(2, 5));
+
+const { add, multiply } = require("./test-module-2");
+console.log(multiply(2, 5));
+
+
+
+// caching
+require("./test-module-3")();
+require("./test-module-3")();
+require("./test-module-3")();

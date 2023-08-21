ba5a2ad8b4813b1cdc14f1d371f6ab95cf3853bb
qiuxan <qiuxan@gmail.com>
2023-08-21 19:43:47 +1000

    Waiting for Multiple Promises Simultaneously

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -27,13 +27,25 @@ const getDogPic = async () => {
         const data = await readFilePro(`${__dirname}/dog.txt`); // it will skip until the readFilePro has a result
         console.log(`Breed: ${data}`);
 
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-        console.log(res.body.message);
 
-        await writeFilePro('dog-img.txt', res.body.message);
+        const res1Pro = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const res2Pro = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+        const res3Pro = superagent.get(
+            `https://dog.ceo/api/breed/${data}/images/random`
+        );
+
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+        const imgs = all.map(el => el.body.message);
+        console.log(imgs);
+
+        await writeFilePro('dog-img.txt', imgs.join('\n'));
         console.log("dog image saved");
     } catch (err) {
-        // console.log("🚀 ~ file: index.js:36 ~ getDogPic ~ err:", err)
+        console.log(err)
         throw err
 
     }
